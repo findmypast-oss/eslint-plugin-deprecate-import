1cec0b29a70ca440907c633dafb534f42955b01a
Emily McDonald <mcdonald.emily@live.co.uk>
2018-06-11 15:42:52 +0100

    :art: Applied prettier to repo

--- tests/lib/rules/module.js
+++ tests/lib/rules/module.js
@@ -1,64 +1,64 @@
-var rule = require("../../../lib/rules/module"),
-  RuleTester = require("eslint").RuleTester;
+var rule = require('../../../lib/rules/module'),
+  RuleTester = require('eslint').RuleTester;
 
 var ruleTester = new RuleTester();
-ruleTester.run("module", rule, {
+ruleTester.run('module', rule, {
   valid: [
     {
       code: "import * as foo from 'supported-module'",
-      options: [{ name: "deprecated-module" }],
-      parser: "babel-eslint"
+      options: [{ name: 'deprecated-module' }],
+      parser: 'babel-eslint',
     },
     {
       code: "import foo from 'supported-module'",
-      options: [{ name: "deprecated-module" }],
-      parser: "babel-eslint"
+      options: [{ name: 'deprecated-module' }],
+      parser: 'babel-eslint',
     },
     {
       code: "import { foo } from 'node-modules/deprecated-module'",
-      options: [{ name: "deprecated-module" }],
-      parser: "babel-eslint"
-    }
+      options: [{ name: 'deprecated-module' }],
+      parser: 'babel-eslint',
+    },
   ],
 
   invalid: [
     {
       code: "import * as foo from 'deprecated-module'",
-      options: [{ name: "deprecated-module", use: "supported-module" }],
+      options: [{ name: 'deprecated-module', use: 'supported-module' }],
       errors: [
         {
           message:
             "Module 'deprecated-module' is deprecated. Use 'supported-module' instead.",
-          type: "Literal"
-        }
+          type: 'Literal',
+        },
       ],
-      parser: "babel-eslint"
+      parser: 'babel-eslint',
     },
     {
       code: "import foo from 'deprecated-module'",
-      options: [{ name: "deprecated-module" }],
+      options: [{ name: 'deprecated-module' }],
       errors: [
         {
           message: "Module 'deprecated-module' is deprecated.",
-          type: "Literal"
-        }
+          type: 'Literal',
+        },
       ],
-      parser: "babel-eslint"
+      parser: 'babel-eslint',
     },
     {
       code: "import { foo } from 'another-deprecated-module'",
       options: [
-        { name: "deprecated-module", use: "supported-module" },
-        { name: "another-deprecated-module", use: "another-supported-module" }
+        { name: 'deprecated-module', use: 'supported-module' },
+        { name: 'another-deprecated-module', use: 'another-supported-module' },
       ],
       errors: [
         {
           message:
             "Module 'another-deprecated-module' is deprecated. Use 'another-supported-module' instead.",
-          type: "Literal"
-        }
+          type: 'Literal',
+        },
       ],
-      parser: "babel-eslint"
-    }
-  ]
+      parser: 'babel-eslint',
+    },
+  ],
 });
